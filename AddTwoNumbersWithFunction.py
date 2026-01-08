6c32006baf5c8ceb792b788699d661c76cbecd0c
Kadhir <139536665+1U23CS053KATHIRAVAN@users.noreply.github.com>
2026-01-08 05:14:55 +0000

    DONE

---
+++ AddTwoNumbersWithFunction.py
@@ -0,0 +1,6 @@
+def add_numbers(a, b):
+    return=a+b
+    num1=(input("Enter first number:"))
+    num2=int(input("Enter second number:"))
+    result=add_numbers(num1,num2)
+    print("the sum of the two numbers is:",result)
